c8dc4993b3653ea086ad875598581672c419db6a
abiyawilson <abiyawilson@gmail.com>
2020-03-06 20:10:07 +0530

    complette

--- app_package/admissionroutes.py
+++ app_package/admissionroutes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for
-from app_package import app, db,mongo
+from app_package import app,mongo
 from flask_login import current_user, login_user, logout_user, login_required
 from app_package.admissionforms import AdmissionSearchForm,AdmissionAddForm
-from app_package.models import User
+
 
 
 
--- app_package/enquiryroutes.py
+++ app_package/enquiryroutes.py
@@ -1,8 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from app_package import app, db,mongo
+from app_package import app,mongo
 from flask_login import current_user, login_user, logout_user, login_required
 from app_package.enquiryforms import EnquiryForm,EnquirySearchForm,EnquiryUpdateForm,EnquiryFilterForm
-from app_package.models import User
 
 
 @app.route("/Enquiryhome",methods=["GET","POST"])
